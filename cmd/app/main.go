5e12c3918f356f76013907774d4cc752bfb0ae2b
Leonardo Peixoto <leonardo.peixoto@fortics.com.br>
2025-10-21 08:32:15 -0300

    refactor: improve command parsing and input handling

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+func parse(raw string) (string, []string) {
+	parts := strings.Split(strings.TrimSpace(raw), " ")
+
+	return parts[0], parts[1:]
+}
+
 func main() {
-	fmt.Print("$ ")
-
-	var cmd string
-
 	for {
-		fmt.Scan(&cmd)
+		fmt.Print("$ ")
+
+		raw, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "there was an error: %v\n", err)
+
+			os.Exit(1)
+		}
+
+		cmd, _ := parse(raw)
+
+		switch cmd {
+		case "exit":
+			os.Exit(0)
+		}
 
 		fmt.Printf("%s: command not found\n", cmd)
 	}
